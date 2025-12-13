30377ddbe3c52d56d8086eb29d69f41142d322a0
Yazhall <mael.vigier@hesias.fr>
2025-12-13 20:03:07 +0100

    add edit/supp thread ,  todo same for comment

--- src/Controller/ThreadController.php
+++ src/Controller/ThreadController.php
@@ -9,11 +9,13 @@ use App\Entity\ThreadFile;
 use App\Form\Type\CommentType;
 use App\Form\Type\ThreadType;
 use App\Service\ThreadFileService;
+use App\Service\ThreadService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class ThreadController extends AbstractController
 {
@@ -67,6 +69,7 @@ class ThreadController extends AbstractController
             'commentForms' => $commentForms,]);
     }
     #[Route('/thread/{id}', name: 'app_thread_show')]
+    #[IsGranted('ROLE_USER')]
     public function show(
         Thread $thread,
         Request $request,
@@ -94,4 +97,70 @@ class ThreadController extends AbstractController
             'commentForm' => $form->createView(),
         ]);
     }
+    #[Route('/thread/{id}/edit', name: 'app_thread_edit')]
+    #[IsGranted('ROLE_USER')]
+    public function edit(
+        Thread $thread,
+        Request $request,
+        ThreadService $threadService,
+        ThreadFileService $threadFileService,
+
+    ): Response {
+
+        $form = $this->createForm(ThreadType::class, $thread);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $uploads = $form->get('uploads')->getData();
+            if ($uploads) {
+                $threadFileService->attachUploadsToThread($thread, $uploads);
+            }
+
+            $threadService->update($thread, $this->getUser());
+
+            return $this->redirectToRoute('app_thread_show', [
+                'id' => $thread->getId()->toRfc4122(),
+            ]);
+        }
+
+        return $this->render('Page/thread/edit_thread.html.twig', [
+            'form' => $form->createView(),
+            'thread' => $thread,
+        ]);
+    }
+    #[Route('/thread/{id}/delete', name: 'app_thread_delete', methods: ['POST'])]
+    #[IsGranted('ROLE_USER')]
+    public function delete(
+        Thread $thread,
+        Request $request,
+        ThreadService $threadService,
+    ): Response {
+
+        if ($this->isCsrfTokenValid('delete_thread_'.$thread->getId(), $request->request->get('_token'))) {
+            $threadService->softDelete($thread, $this->getUser());
+        }
+
+        return $this->redirectToRoute('app_feed_thread');
+    }
+    #[Route('/my_thread_liste', name: 'app_my_thread_liste')]
+    public function showMyThreadListe(
+        Request $request,
+        EntityManagerInterface $entityManager,
+    ): Response
+    {
+        $threads = $entityManager->getRepository(Thread::class)->findBy([
+            'author' => $this->getUser(),
+            'isDeleted' => false,
+            ],
+            ['createdAt' => 'DESC']);
+        return $this->render('Page/thread/my_thread_list.html.twig', [
+            'threads' => $threads,
+        ]);
+
+    }
+
+
+
+
 }
